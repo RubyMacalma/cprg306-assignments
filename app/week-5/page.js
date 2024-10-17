d2e970565387a6fedceb0bf1065b38bf7f064388
RubyMacalma <152216379+RubyMacalma@users.noreply.github.com>
2024-10-17 01:52:45 -0600

    update

---
+++ app/week-5/page.js
@@ -0,0 +1,10 @@
+import NewItem from "./new-item";
+
+export default function Page () {
+
+return (
+      <main class= " flex justify-center w-full">
+        <NewItem></NewItem>
+      </main>
+    );
+  }
