2bb3b1e868007fa978947ee2c4746894345383a7
RubyMacalma <152216379+RubyMacalma@users.noreply.github.com>
2024-10-22 22:36:04 -0600

    update

---
+++ app/week-6/item.js
@@ -0,0 +1,10 @@
+
+export default function Item({name,quantity,category}){
+    return(
+       <li className="p-2 m-4 bg-slate-900 max-w-sm">
+        <div>{name}</div>
+        <div>buy {quantity} in {category}</div>
+       </li>
+
+    );
+};
